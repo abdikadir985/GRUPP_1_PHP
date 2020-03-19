2f70461d1e740cf220c20a0d7e322a93d2654795
taskmaster <60099108+abdikadir985@users.noreply.github.com>
2020-03-19 16:14:48 +0100

    Update Dicewithsession.php

--- Dicewithsession.php
+++ Dicewithsession.php
@@ -14,12 +14,40 @@
 		<div>
 			<?php
 				//Var uppmärksam på att PHP-tolken används på ett flertal ställen i filen!
-				session_regenerate_id(true);
-				$newGame="";
-				function newGame(){
-					$newGame="New Game!";
-					echo $newGame;
-				}
+				//Var uppmärksam på att PHP-tolken används på ett flertal ställen i filen!
+		include("include/OneDice.php");
+		include("include/SixDices.php");
+
+
+		$disabled = true;
+
+		if(isset($_GET["linkNewGame"])){
+    // 1 a
+			echo("New Game!");
+   // 1 b
+  	setcookie("nbrOfRounds",0, time() + 3600);
+	  setcookie("sumOfAllRounds",0, time() + 3600);
+
+			$disabled = false;
+		}
+    // 1 c, 3
+		if(isset( $_GET["btnRoll"]) &&
+		isset($_SESSION["linkRoll"]) &&
+		isset($_SESSION["linkExit"])){
+		$nbr = $_SESSION["nbrOfRounds"];
+		$sum = $_SESSION["sumOfAllRounds"];
+		echo("Button Rolled");
+
+
+    $disabled = false;
+		}
+    // 2
+		if(!isset($_GET["linkExit"]) &&
+		isset($_SESSION["nbrOfRounds"]) &&
+		isset($_SESSION["sumOfAllRounds"])) {
+		$nbr = $_SESSION["nbrOfRounds"];
+		$sum = $_SESSION["sumOfAllRounds"];
+		echo (...);
 			?>
 		</div>
 		
