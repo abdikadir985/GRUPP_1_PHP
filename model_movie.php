9db8791a10285e8aa0e600c0021ceddbaf90a19a
taskmaster <60099108+abdikadir985@users.noreply.github.com>
2020-03-27 16:19:11 +0100

    Update model_movie.php

--- model_movie.php
+++ model_movie.php
@@ -1,1 +1,15 @@
+<?php
+// lab 2 gruppmeddlemar [Abdikadir Omar, Hoseop Joung & Blend Zebari]
+namespace App;
 
+use Illuminate\Database\Eloquent\Model;
+
+class movie extends Model {
+
+  protected $tabel = 'movies';
+
+  protected $fillable = ['title','year','genre', 'rating'];
+
+}
+
+ ?>
