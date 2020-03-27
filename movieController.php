a2fdd2eac49a51a2d8d1f19fd68c7ff497375ac2
taskmaster <60099108+abdikadir985@users.noreply.github.com>
2020-03-27 16:30:27 +0100

    Update movieController.php

--- movieController.php
+++ movieController.php
@@ -1,1 +1,19 @@
+<?php
+// lab2 / Ramverk/ Gruppmeddlemar: [Abdikadir Omar, Hoseop Joung, & Blend zebari]
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+use Illuminate\Validation\Rule;
+
+class movieController extends Controller {
+  
+    public function __index()  {
+
+      $Posts = movie::all();
+
+      return $Posts['']-title;
+      
+    }
+}
