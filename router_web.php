635e086314048dc9e76af2ad000daf5723333623
taskmaster <60099108+abdikadir985@users.noreply.github.com>
2020-03-27 16:23:05 +0100

    Update router_web.php

--- router_web.php
+++ router_web.php
@@ -1,1 +1,25 @@
+<?php
+
+// lab 2 Ramverk/ Gruppmeddlemar: [Abdikadir Omar, Hoseop Joung, & Blend Zebari]
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('Movie','MoviesController@index');
+
+
+$router->get('Movie/{id}','MoviesController@movieid');
 
