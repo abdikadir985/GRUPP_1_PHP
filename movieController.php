08a59eba14db37c02563affa361879452f06a508
taskmaster <60099108+abdikadir985@users.noreply.github.com>
2020-04-02 15:53:05 +0200

    Update movieController.php

--- movieController.php
+++ movieController.php
@@ -8,20 +8,74 @@ use Illuminate\Validation\Rule;
 
 class moviesController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     *
-     */
-    public function index()  {
 
-      $Posts = Movie::all();
+  public function index()  {
 
-      return $Posts;
+    $Posts = Movie::all();
 
-    }
+    return $Posts;
+
+  }
+  public function getByregnr ($regnr){
+
+    $Posts = bil::where('regnr','=',$regnr)->firstorFail();
+
+    return $Posts;
+  }
+
+  public function create(Request $request) {
+
+    $data=  $this->validate($request, [
+      'title' => 'string|required',
+      'year' => 'integer',
+      'genre' => 'string',
+      'rating' => 'integer|min:1|max:10',
+    ]);
+
+    $data = $request->all();
+
+    $Posts = new Movie();
+
+    $Posts->fill($data);
+
+    $Posts->save();
+
+    return ['success' => true,'id' => $Posts->id,];
+  }
+
+  public function read($id) {
 
 
+    $Posts = Movie::findOrFail($id);
 
+    return $Posts;
+
+  }
+
+  public function update(Request $request,$id){
+
+    $data=  $this->validate($request, [
+      'title' => 'string|filled',
+      'year' => 'integer',
+      'genre' => 'string',
+      'rating' => 'integer|min:1|max:10',
+    ]);
+
+    $Posts = Movie::findOrFail($id);
+    $Posts-> fill($data);
+    $Posts ->save();
+
+    return ['success'=> true,];
+
+  }
+
+  public function delete($id){
+
+    $Posts= Movie::findorFail($id);
+    $Posts-> delete();
+
+    return ['success' => true,];
+
+  }
 
 }
