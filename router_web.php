567044a39f537a1eae638cc3fa0389c554696241
RetardWorks <61974688+RetardWorks@users.noreply.github.com>
2020-04-02 16:57:53 +0200

    Update router_web.php

--- router_web.php
+++ router_web.php
@@ -10,14 +10,21 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
+//registrerar urlen i lumen
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-
+//testar och ser om JSON funkar
 $router->get('test','MoviesController@test');
+//hämtar själva indexen till movies (allting sorterat.)
+//exempel: (http://localhost/lumen/public/movies)
 $router->get('movies','MoviesController@index');
+//låter dig läsa upp en movie utefter dess id
+//exempel: (http://localhost/lumen/public/movies/1)
 $router->get('movies/{id}','MoviesController@read');
+//denna låter dig lägga till nya filmer i databasen
 $router->post('movies','MoviesController@create');
+//denna låter dig uppdatera den nuvarande innehåll inom id:n till något nytt.
 $router->put('movies/{id}','MoviesController@update');
+//denna låter dig ta bort själva id:t och dess innehåll helt och hållet.
 $router->delete('movies/{id}','MoviesController@delete');
